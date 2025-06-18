1f501d4a8d13c494ad90fa1654af71e93033172e
Gustavo Luna Procopio <gugalunaprocopio@gmail.com>
2025-06-17 23:51:53 -0300

    Adicionando Dropdown para filtro de loja

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = Dash(__name__)
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.read_excel("Vendas.xlsx")
+options = list(df['ID Loja'].unique())
+options.append("Todas as Lojas")
 
 # criando o gráfico
 fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
@@ -16,8 +18,10 @@ app.layout = html.Div(children=[
     html.H2(children='Gráfico com faturamento de todos os produtos separados por loja'),
     html.Div(children='OBS: Gráfico mostra quantidade de produtos vendidos, não o faturamento'),
 
+    dcc.Dropdown(options, value='Todas as Lojas', id='lista_Lojas'),
+
     dcc.Graph(
-        id='example-graph',
+        id='grafico_qtde_vendas',
         figure=fig
     )
 ])
